4303847ed3c19b0dd05d5217771589cb428bf44a
chandruk <chandruk@localhost>
2025-09-23 12:15:34 +0530

    i am commited

---
+++ CT2C_JAVADAY1/src/package1/Chandruk.java
@@ -0,0 +1,10 @@
+package package1;
+
+public class Chandruk {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("Vanakkam da mapley");
+	}
+
+}
