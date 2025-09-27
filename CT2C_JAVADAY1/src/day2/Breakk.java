8b9a1c0b922e5add772e2566f16e8ff831213346
chandruk <chandruk@localhost>
2025-09-27 14:08:56 +0530

    day 2 Completed

---
+++ CT2C_JAVADAY1/src/day2/Breakk.java
@@ -0,0 +1,10 @@
+package day2;
+
+public class Breakk {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
---
+++ CT2C_JAVADAY1/src/day2/Continue.java
@@ -0,0 +1,9 @@
+package day2;
+
+public class Continue {
+
+	public static void main(String[] args) {
+		
+	}
+
+}
